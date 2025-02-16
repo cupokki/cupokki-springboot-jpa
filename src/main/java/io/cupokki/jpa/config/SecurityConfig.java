d901369388245635eda5439c6ff30b3ab23024a1
cupokki <nd1346@gmail.com>
2025-02-16 23:53:45 +0900

    MemberService 추가

---
+++ src/main/java/io/cupokki/jpa/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package io.cupokki.jpa.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+}
---
+++ src/main/java/io/cupokki/jpa/config/SpringConfig.java
@@ -0,0 +1,12 @@
+package io.cupokki.jpa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+
+//    @Bean
+    // 레포지토리, 서비스 변경
+
+}
