f9dbddd1122bf88dc3e2b1db2469c34de6b371ed
Jung <nd1346@gmail.com>
2025-02-27 09:07:03 +0900

    PasswordUtil

---
+++ src/main/java/io/cupokki/webmvcboilerplate/util/PasswordUtil.java
@@ -0,0 +1,14 @@
+package io.cupokki.webmvcboilerplate.util;
+
+public class PasswordUtil {
+
+    static String encrypt(String plain) {
+        String hashed = "";
+
+        return hashed;
+    }
+
+    static boolean match(String a, String b) {
+        return false;
+    }
+}
